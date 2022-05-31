856ab31b9ab379a703c99788e7286b801856734b
Justin Strang <Justinstrang100@gmail.com>
2022-05-31 12:36:25 -0400

    Event listener for board elements

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const gameBoard = (() => {
     const board = [
-        ['X','X'],
-        ['X','X'],
-        ['X','X']
+        ['X','X','X'],
+        ['X','X','X'],
+        ['X','X','X']
     ];
     const gameBoardContainer = document.querySelector('.gameBoardContainer');
     for (element in board) {
         const newElement = document.createElement('div');
         newElement.innerHTML = board[element];
         gameBoardContainer.appendChild(newElement);
+        newElement.addEventListener('click', () => {console.log('Hello')})
     }
 })();
 
