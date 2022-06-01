9c27542a95bf039ebcccd33f8c1b6bae66dfeb23
Justin Strang <Justinstrang100@gmail.com>
2022-06-01 10:47:20 -0400

    Player click function works

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 //Module which creates gameboard array with clickable elements
+const newPlayer = (playerNumber) => {
+    let playerSign;
+    if (playerNumber == 0) {
+        playerSign = 'X';
+    }
+    else if (playerNumber == 1) {
+        playerSign = 'O';
+    }
+    else {console.log('Error: maximum player count is 2 players')}
+
+    const playerClick = (e) => {
+        e.target.innerHTML = playerSign;
+    }
+    return {playerClick}
+}
+const playerOne = newPlayer(0);
 
 const gameBoard = (() => {
     const board = [
@@ -12,34 +28,15 @@ const gameBoard = (() => {
     for (i = 0; i < board.length; i++) {
         for (j = 0; j < board[i].length; j++) {
             const newElement = document.createElement('div');
-            newElement.setAttribute('class', 'sqaures');
+            newElement.setAttribute('class', 'squares');
             newElement.setAttribute('id', [i] + [j]);
             newElement.innerHTML = board[i][j];
             gameBoardContainer.appendChild(newElement);
+
+            newElement.addEventListener('click', playerOne.playerClick.bind(newElement));
         }    
     }
 })();
 
-const newPlayer = (playerNumber) => {
-    let playerSign;
-    if (playerNumber == 0) {
-        playerSign = 'X';
-    }
-    else if (playerNumber == 1) {
-        playerSign = 'O';
-    }
-    else {console.log('Error: maximum player count is 2 players')}
 
-    const playerClick = () => {
-        
-    }
-    return {playerClick}
-}
-
-const playerOne = newPlayer(0);
-
-
-
-const displayController = () {
-
-}
+//Must allow unclicked element to become clicked and then switch to other player
