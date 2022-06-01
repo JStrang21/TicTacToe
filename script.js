6f697c0c2991378d5b2450c5e08aba88f85ff2c6
Justin Strang <Justinstrang100@gmail.com>
2022-06-01 11:11:48 -0400

    User clicks work

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Module which creates gameboard array with clickable elements
+
 const newPlayer = (playerNumber) => {
     let playerSign;
     if (playerNumber == 0) {
@@ -15,14 +15,15 @@ const newPlayer = (playerNumber) => {
     return {playerClick}
 }
 const playerOne = newPlayer(0);
+const playerTwo = newPlayer(1);
 
+//Module which creates gameboard array with clickable elements
 const gameBoard = (() => {
     const board = [
         ['','',''],
         ['','',''],
         ['','','']
     ];
-
     const gameBoardContainer = document.querySelector('.gameBoardContainer');
     //Two loops for each dimension of board array
     for (i = 0; i < board.length; i++) {
@@ -33,10 +34,38 @@ const gameBoard = (() => {
             newElement.innerHTML = board[i][j];
             gameBoardContainer.appendChild(newElement);
 
-            newElement.addEventListener('click', playerOne.playerClick.bind(newElement));
+            //newElement.addEventListener('click', playerOne.playerClick.bind(newElement));
+            newElement.addEventListener('click', countClicks.bind(newElement));
         }    
     }
 })();
 
+let numberOfClicks = 0;
+function countClicks(e) {
+    if (numberOfClicks < 9) {
+        if (numberOfClicks % 2 == 0) {
+            numberOfClicks++;
+            playerOne.playerClick(e);
+        }
+        else {
+            numberOfClicks++;
+            playerTwo.playerClick(e);
+        }
+    }
+}
+
 
 //Must allow unclicked element to become clicked and then switch to other player
+/*function switchPlayers(e) {
+    for (i=0; i<9;) {
+        if (i % 2 == 0) {
+            let returned = playerOne.playerClick.bind(e);
+            i++;
+            return returned
+        }
+        else {
+            i++;
+            return console.log('No')
+        }
+    }
+}*/
